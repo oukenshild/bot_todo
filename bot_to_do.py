88e15bf024c8889a2780fe4381d061b3e311095f
Danil <danililukhin@yandex.ru>
2022-11-07 22:51:11 +0500

    Files added

--- bot_to_do.py
+++ bot_to_do.py
@@ -1,59 +1,59 @@
 import random
+from package_data.data import help, tasks, random_tasks
 
-HELP = """
-help — напечатать справку по программе.
-add — добавить задачу в список (название задачи запрашиваем у пользователя).
-show — напечатать все добавленные задачи.
-exit — выйти из программы
-random — добавить случайную задачу на дату Сегодня"""
 
-RANDOM_TASKS = [
-  'Записаться на курс Нетологии', 'Написать Гвидо письмо', 'Покормить кошку', 'Помыть машину'
-]
+class Todolist:
+    def __init__(self, date, task):
+        self.date = date
+        self.task = task
 
-tasks = {}
+    @classmethod
+    def help_todo(cls):
+        print(help)
 
-# Сегодня, Завтра, 31.12 ...
-# [Задача1, Задача2, Задача3]
-# Дата -> [Задача1, Задача2, Задача3]
+    def add_todo(self, date, task):
+        if date in tasks:
+            # Дата есть в словаре
+            # Добавляем в список задачу
+            tasks[date].append(task)
+        else:
+            # Даты в словаре нет
+            # Создаем запись с ключом date
+            tasks[date] = []
+            tasks[date].append(task)
+        print('Задача', task, 'добавлена на дату', date)
 
-run = True
 
-def add_todo(date, task):
-  if date in tasks:
-      # Дата есть в словаре
-      # Добавляем в список задачу
-      tasks[date].append(task)
-  else:
-      # Даты в словаре нет
-      # Создаем запись с ключом date
-      tasks[date] = []
-      tasks[date].append(task)
-  print('Задача', task, 'добавлена на дату', date)
+def main():
+    todolist = Todolist('26 ноября', 'Помыть голову')
+    print('Приветствую!')
+    while True:
+        command = input('Введите команду: ')
+        if command == 'help':
+            todolist.help_todo()
+        elif command == 'show':
+            date = input('Введите дату для отображения списка задач: ')
+            if date in tasks:
+                for task in tasks[date]:
+                    print('- ', task)
+            else:
+                print('Такой даты нет')
+        elif command == 'add':
+            date = input('Введите дату для добавления задачи: ')
+            task = input('Введите название задачи: ')
+            todolist.add_todo()
+        elif command == 'exit':
+            print('Спасибо за использование! До свидания!')
+            break
+        elif command == 'random':
+            task = random.choice(random_tasks)
+            todolist.add_todo('Сегодня', task)
+        elif command == 'random_date':
+            todolist.add_todo(date, random_tasks)
+        else:
+            print('Неизвестная команда')
+            break
 
-while run:
-  command = input('Введите команду: ')
-  if command == 'help':
-    print(HELP)
-  elif command == 'show':
-    date = input('Введите дату для отображения списка задач: ')
-    if date in tasks:
-      for task in tasks[date]:
-        print('- ', task)
-    else:
-      print('Такой даты нет')
-  elif command == 'add':
-    date = input('Введите дату для добавления задачи: ')
-    task = input('Введите название задачи: ')
-    add_todo(date, task)
-  elif command == 'exit':
-    print('Спасибо за использование! До свидания!')
-    break
-  elif command == 'random':
-    task = random.choice(RANDOM_TASKS)
-    add_todo('Сегодня', task)
-  elif command == 'random_date':
-    add_todo(date, RANDOM_TASKS)
-  else:
-    print('Неизвестная команда')
-    break
+
+if __name__ == '__main__':
+    main()
